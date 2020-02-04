bba58fd07ad703e4564da3ed0bc3f8be60fd8271
DivyaYadavareddy <60208911+DivyaYadavareddy@users.noreply.github.com>
2020-02-04 16:55:06 +0530

    Update diplayvideo.py

--- diplayvideo.py
+++ diplayvideo.py
@@ -1,8 +1,9 @@
+import numpy as np
 import cv2
 import imutils
 from PIL import Image
 from pytesseract import *
-import numpy as np
+
 # video_capture = cv2.VideoCapture(0)
 
 # For real-time sample video detection
@@ -16,7 +17,7 @@ print('Total number of frames : ' + str(frame_count))
 while True:
     ret, frame = video_capture.read()
     plate_cascade = cv2.CascadeClassifier('./haarcascade_russian_plate_number.xml')
-    frame = imutils.resize(frame, width=1000) # resize original video for better viewing performance
+    # frame = imutils.resize(frame, width=1000) # resize original video for better viewing performance
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert video to grayscale
 
     plate_rect = plate_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=7)
@@ -26,35 +27,25 @@ while True:
         plate = frame[y + a:y + h - a, x + b:x + w - b, :]
         # finally representing the detected contours by drawing rectangles around the edges.
         cv2.rectangle(frame, (x, y), (x + w, y + h), (51, 51, 255), 3)
+
         time = video_capture .get(cv2.CAP_PROP_POS_MSEC)
         time = time / 1000
         frame_no = video_capture .get(cv2.CAP_PROP_POS_FRAMES)
-        if frame_no == 30 :
-            print("number plate detected at frame:", plate_rect, "Frame number:", frame_no, "Time:", time)
-            import cv2
-
-            if __name__ == '__main__':
-                # Read image
-                # Select ROI
-                r = cv2.selectROI(frame)
-
-                # Crop image
-                imCrop = frame[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
-
-                # Display cropped image
-                cv2.imshow("Image", imCrop)
-                cv2.waitKey(0)
-                # im = Image.open('index.jpeg')"""
-                imgCrop = cv2.resize(imCrop, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
-                sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-                sharpen = cv2.filter2D(imgCrop, -1, sharpen_kernel)
-                text = image_to_string(imCrop)
-                print("number plate is:")
-                print("********")
-                print(text)
-                print("********")
-                Dict = dict({"Detected at Frame No": frame_no, "Time": time})
-                print(Dict)
+        print("number plate detected at frame:", plate_rect, "Frame number:", frame_no, "Time:", time)
+        # Now crop
+        Cropped = frame[y:y+h, x:x+w]
+        cv2.imshow("cropped", Cropped)
+        # Read the number plate
+        Cropped = cv2.resize(Cropped, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+        sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        sharpen = cv2.filter2D(Cropped, -1, sharpen_kernel)
+        text = pytesseract.image_to_string(sharpen, config='--psm 11')
+        print("number plate is:")
+        print("**************************************")
+        print(text)
+        print("**************************************")
+        Dict = dict({"Detected at Frame No": frame_no, "Time": time})
+        print(Dict)
     cv2.imshow('video',frame)
     # stop script when "q" key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
